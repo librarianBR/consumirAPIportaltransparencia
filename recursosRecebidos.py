6e5267c2cb0e4e78b984f0fa880e5a35ea60ae86
librarianBR <89207747+librarianBR@users.noreply.github.com>
2022-06-21 18:02:04 -0300

    Update recursosRecebidos.py

--- recursosRecebidos.py
+++ recursosRecebidos.py
@@ -6,12 +6,13 @@ import json
 import pandas as pd
 import time
 
+
 url = "http://api.portaldatransparencia.gov.br/api-de-dados/despesas/recursos-recebidos"
 codigoFavorecido = input('Informe o CNPJ do favorecido XX.XXX.XXX/XXXX-XX-> ') # 76.659.820/0001-51
 
-mesAnoInicio = input('Informe o mês e ano inicial com o seguinte formato: MM/AAAA')
-mesAnoFim = input('Informe o mês e ano final com o seguinte formato: MM/AAAA')
-uf = input('Informe o código da Unidade Federativa em maiusculo XX')
+mesAnoInicio = input('Informe o mês e ano inicial com o seguinte formato: MM/AAAA -> ')
+mesAnoFim = input('Informe o mês e ano final com o seguinte formato: MM/AAAA -> ')
+uf = input('Informe o código da Unidade Federativa em maiusculo XX -> ')
 # SIAF: Ministério da educação : 26000; http://www.planalto.gov.br/ccivil_03/_ato2004-2006/2004/lei/anexos/lei10933-04/10933Anexo_III.pdf
 
 orgaoSuperior = input('informe o código do orgão superior: ex. MEC: 26000'\n "-> ")
